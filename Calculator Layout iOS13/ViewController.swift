eda500764fc592b2757d0ee28d1c6b0b5c6f645b
asmaa eliwa <asmaagamalelsayed96@gmail.com>
2024-07-05 11:17:57 -0400

    functionalites done

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -9,12 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var ResultLabel: UILabel!
+    var firstNum = ""
+    var secNum = ""
+    var result:Float = 0.0
+    var isFinishTyping = false
+    var finish = false
+    var operation:String?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func MethodPressed(_ sender: UIButton) {
+        result = Float(firstNum) ?? 0.0
+       
+        
+        if sender.currentTitle == "AC" {
+            
+            firstNum = ""
+            secNum = ""
+            result = 0
+            ResultLabel.text = String(result)
+        }
+            else if sender.currentTitle != "=" {
+                isFinishTyping = true
+            operation = sender.currentTitle
 
+        }
+        else {
+            isFinishTyping = false
+            switch operation{
+            case "+":
+                result+=Float(secNum)!
+                
+                print("add")
+            case "-":
+                print("-")
+                result-=Float(secNum) ?? 0.0
+            case "%":
+                result/=Float(secNum) ?? 0.0
+            case "*":
+                result*=Float(secNum) ?? 0.0
+             default:
+                print("not recognized")
+            }
+            ResultLabel.text = String(result)
+        }
+    
+    }
+    
+    @IBAction func NumberPressed(_ sender: UIButton) {
+        if isFinishTyping {
+            secNum += sender.currentTitle ?? ""
+            ResultLabel.text = secNum
+        }else{
+            firstNum += sender.currentTitle ?? ""
+            ResultLabel.text = firstNum
+        }
+    }
 }
 
